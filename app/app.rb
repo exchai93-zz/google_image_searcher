0719b93af4bf87b68a99132fb0290bc797039a37
Emily Chai <echai93@gmail.com>
2017-06-20 10:58:07 +0100

    Add routes for coloured searches

--- app/app.rb
+++ app/app.rb
@@ -8,4 +8,33 @@ class Search < Sinatra::Base
     erb :'searches/index'
   end
 
+  get '/searches/red' do
+    erb :'searches/red'
+  end
+
+  get '/searches/pink' do
+    erb :'searches/pink'
+  end
+
+  get '/searches/orange' do
+    erb :'searches/orange'
+  end
+
+  get '/searches/yellow' do
+    erb :'searches/yellow'
+  end
+
+  get '/searches/green' do
+    erb :'searches/green'
+  end
+
+  get '/searches/blue' do
+    erb :'searches/blue'
+  end
+
+  get '/searches/purple' do
+    erb :'searches/purple'
+  end
+
+
 end
