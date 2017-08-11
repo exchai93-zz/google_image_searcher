939e2bc5c8fe471dcee9210046eb3db599bd8eba
Emily Chai <echai93@gmail.com>
2017-08-11 15:54:49 +0100

    Remove separate routes for each colour, use colour params instead

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,7 @@
 ENV['RACK_ENV']||= 'development'
+
+require 'net/http'
+require 'uri'
 require 'sinatra/base'
 require 'sinatra/flash'
 
@@ -8,33 +11,9 @@ class Search < Sinatra::Base
     erb :'searches/index'
   end
 
-  get '/searches/red' do
-    erb :'searches/red'
+  get '/searches/:colour' do
+    @color = params[:colour]
+    erb :'searches/colour', locals: { colour: @colour}
   end
-
-  get '/searches/pink' do
-    erb :'searches/pink'
-  end
-
-  get '/searches/orange' do
-    erb :'searches/orange'
-  end
-
-  get '/searches/yellow' do
-    erb :'searches/yellow'
-  end
-
-  get '/searches/green' do
-    erb :'searches/green'
-  end
-
-  get '/searches/blue' do
-    erb :'searches/blue'
-  end
-
-  get '/searches/purple' do
-    erb :'searches/purple'
-  end
-
 
 end
