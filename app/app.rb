d503b392d83d5a0009193aac80b6e2602688060e
Emily Chai <echai93@gmail.com>
2017-08-11 15:58:24 +0100

    Create array with search terms

--- app/app.rb
+++ app/app.rb
@@ -13,6 +13,9 @@ class Search < Sinatra::Base
 
   get '/searches/:colour' do
     @color = params[:colour]
+    array = [man, mountain, state, ocean, country, building, cat, airline, wealth,  happiness, pride, fear, religion, bird, book, phone, rice, snow, water]
+    random_term = array.sample
+    
     erb :'searches/colour', locals: { colour: @colour}
   end
 
