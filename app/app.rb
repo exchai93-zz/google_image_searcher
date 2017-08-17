1c1a5761494cef14ae4448029255b1b80caedd6c
Emily Chai <echai93@gmail.com>
2017-08-17 08:55:03 +0100

    /searches/:colour queries with a colour and random_term

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,8 @@ ENV['RACK_ENV']||= 'development'
 
 require 'net/http'
 require 'uri'
+require 'json'
+require 'ostruct'
 require 'sinatra/base'
 require 'sinatra/flash'
 
@@ -12,11 +14,17 @@ class Search < Sinatra::Base
   end
 
   get '/searches/:colour' do
-    @color = params[:colour]
-    array = [man, mountain, state, ocean, country, building, cat, airline, wealth,  happiness, pride, fear, religion, bird, book, phone, rice, snow, water]
+    @colour = params[:colour]
+    array = %w( man mountain state ocean country building cat airline wealth
+                happiness pride fear religion bird book phone rice snow water)
+
     random_term = array.sample
-    
-    erb :'searches/colour', locals: { colour: @colour}
+    url = "https://pixabay.com/api/?key=5878850-33e21954d0b8f9303b1beda0c&q=#{@colour}+#{random_term}&image_type=photo"
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    @images = JSON.parse(response.body, object_class: OpenStruct).hits
+
+    erb :'searches/colour', locals: { colour: @colour.to_s, images: @images }
   end
 
 end
